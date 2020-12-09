3f72f9d52cc8f10a2444222e87440fad049b70b3
Carla Rodríguez <crodriguez42@ucol.mx>
2020-12-09 14:00:10 -0600

    implemented sumatoriaSerieUno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-export default class App {}
+export default class App {
+  sumatoriaSerieUno(numero) {
+    let suma = 1;
+    for (let i = 2; i <= numero; i++) {
+      suma = suma + 1 / i;
+    }
+    return suma;
+  }
+}
 
 let app = new App();
+console.log(app.sumatoriaSerieUno(7));
