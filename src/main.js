bee519ac43e7558a9abc5769e88d72e9dfd9233d
Carla Rodríguez <crodriguez42@ucol.mx>
2020-12-10 14:29:02 -0600

    implemented obtenerImpares

--- src/main.js
+++ src/main.js
@@ -41,6 +41,41 @@ export default class App {
       return false;
     }
   }
+
+  obtenerMultiplos(inicio, fin) {
+    let multiplos = "";
+    do {
+      if (inicio % 3 === 0) {
+        multiplos = multiplos + inicio;
+        inicio++;
+      } else {
+        inicio++;
+      }
+    } while (inicio <= fin);
+
+    return multiplos;
+  }
+
+  obtenerImpares(numero1, numero2) {
+    let resultado = "";
+
+    if (numero1 > numero2) {
+      let t = numero1;
+      numero1 = numero2;
+      numero2 = t;
+    }
+
+    do {
+      if (numero2 % 2 !== 0) {
+        resultado = resultado + numero2 + ",";
+        numero2--;
+      } else {
+        numero2--;
+      }
+    } while (numero2 >= numero1);
+
+    return resultado;
+  }
 }
 
 let app = new App();
@@ -48,3 +83,6 @@ console.log(app.sumatoriaSerieUno(7));
 console.log(app.sumatoriaSerieDos(7));
 console.log(app.esPrimo(9));
 console.log(app.esPrimo(3));
+console.log(app.obtenerMultiplos(10, 25));
+console.log(app.obtenerImpares(5, 10));
+console.log(app.obtenerImpares(10, 5));
